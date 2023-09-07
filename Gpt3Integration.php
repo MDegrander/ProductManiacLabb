0a65a8f9e74f16b02de82cee8b88e02dd01dfb25
MDegrander <128405096+MDegrander@users.noreply.github.com>
2023-09-07 20:03:15 +0200

    Update Gpt3Integration.php

--- Gpt3Integration.php
+++ Gpt3Integration.php
@@ -1,22 +1,28 @@
+<?php
 // GPT3Integration.php
 
-require 'vendor/autoload.php';  // Om du använder Composer för pakethantering
+// Inkludera Composer autoloader
+require_once 'vendor/autoload.php';
 
 class Gpt3Integration {
-private $api_key;
+    // Deklarera en privat variabel för API-nyckeln
+    private $api_key;
 
-public function __construct() {
-    $this->api_key = get_option('openai_api_key', '');
-}
+    // Konstruktor
+    public function __construct() {
+        $this->api_key = get_option('openai_api_key', '');
+    }
 
-
+    // Funktion för att analysera text med OpenAI API
     public function analyzeText($text) {
-        // Använd OpenAI API för att analysera texten
-        // Returnera analyserade data
+        // Här kan du lägga till kod för att anropa OpenAI API
+        // och returnera analyserade data
     }
 
+    // Funktion för att omskriva text med OpenAI API
     public function rewriteText($text) {
-        // Använd OpenAI API för att omskriva texten
-        // Returnera den omskrivna texten
+        // Här kan du lägga till kod för att anropa OpenAI API
+        // och returnera den omskrivna texten
     }
 }
+
